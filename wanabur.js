5156d94039453e8116bdd1019a805a002523ffad
Walburr <145846387+Walburr@users.noreply.github.com>
2024-01-03 14:41:09 +0100

    barre de recherche

--- wanabur.js
+++ wanabur.js
@@ -1,8 +1,11 @@
-function chercher_vt(){
-    var a = document.getElementById("search-bar").value;
+function chercher() {
+    var a = document.getElementById("search-bar").value.toLowerCase();
 
-    if(a === "vortex" ,"vt" , "Vortex" , "VT" , "VORTEX" , "VORETEX" , "vT" , "Vt" , "voretex" , "modder", "VOrtex" , "VORtex" , "VORTex" , "VORTEx"){
-        window.open("/vortex.html");
+    if (["jeb", "japne", "japan", "japen", "japen 19", "japen 19th"].includes(a)) {
+        window.open("japen.html");
     }
 
+    if (["vortex", "vt", "Vortex", "VT", "VORTEX", "VORETEX", "vT", "Vt", "voretex", "modder", "VOrtex", "VORtex", "VORTex", "VORTEx"].includes(a)) {
+        window.open("vortex.html");
+    }
 }
