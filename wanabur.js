cefebdb722570e40370285dc4d0996ba45b2fc23
Walburr <145846387+Walburr@users.noreply.github.com>
2024-01-03 13:22:04 +0100

    barre de recherche en js

--- wanabur.js
+++ wanabur.js
@@ -1,7 +1,7 @@
 function chercher_vt(){
     var a = document.getElementById("search-bar").value;
 
-    if(a === "vortex" ,"vt" , "Vortex" , "VT" , "VORTEX" , "VORETEX" , "vT" , "Vt" , "voretex" , "modder"){
+    if(a === "vortex" ,"vt" , "Vortex" , "VT" , "VORTEX" , "VORETEX" , "vT" , "Vt" , "voretex" , "modder", "VOrtex" , "VORtex" , "VORTex" , "VORTEx"){
         window.open("/vortex.html");
     }
 
