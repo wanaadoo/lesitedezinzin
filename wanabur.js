5476cba659d2eb6680efa5878c95367c35e58d18
Walburr <145846387+Walburr@users.noreply.github.com>
2024-01-03 12:26:47 +0100

    romain aime les bites de blanc
    bite

---
+++ wanabur.js
@@ -0,0 +1,8 @@
+function chercher_vt(){
+    var a = document.getElementById("search-bar").value;
+
+    if(a === "vortex" ,"vt" , "Vortex" , "VT" , "VORTEX" , "VORETEX" , "vT" , "Vt" , "voretex" , "modder"){
+        window.open("/vortex.html");
+    }
+
+}
